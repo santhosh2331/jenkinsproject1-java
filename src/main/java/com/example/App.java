a7ab683a01c41c0cd320d26207f6a77c7d203c15
SANTHOSH <sandysanthosh991@gmail.com>
2026-04-08 11:00:16 +0530

    Update print statement in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins my webhook trigger ");
+        System.out.println("Hello, Jenkins my webhook trigger 1 ");
     }
 
     public int add(int a, int b) {
